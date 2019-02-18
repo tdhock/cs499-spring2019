27d7b434b20d900484d63c9fd066469a94930fc6
Toby Dylan Hocking <tdhock5@gmail.com>
2019-02-18 12:39:24 -0700

    Update project-1-test.R

--- project-1-test.R
+++ project-1-test.R
@@ -1,3 +1,24 @@
+ You can make more columns if you think that will help you do the grading. After filling it out please email me to schedule a meeting to discuss the overall grades. And please email me if you have any questions after talking with me tomorrow.
+
+The rubric for grading is here https://github.com/tdhock/cs499-spring2019/blob/master/project-1-nearest-neighbors.org#grading-rubric-100-points
+
+You should start by cloning each github repo to your computer. Then do "git checkout" to go back to the most recent commit on Fri Feb 15 (commits after the deadline should be ignored for grading).
+
+Here is the link to the R code that you should run on each group's submission for the "accuracy" part, https://github.com/tdhock/cs499-spring2019/blob/master/project-1-test.R
+
+For the "report" part you may need to add "VignetteBuilder: knitr" to the DESCRIPTION file, because it seems that most groups forgot that. Otherwise open the vignettes/report.Rmd file in Rstudio and grade based on that.
+
+For the "win-builder" part you will need to first put your email in the Maintainer field in DESCRIPTION, and then do R CMD build to get a tar.gz file, and then upload it to https://win-builder.r-project.org/upload.aspx, and then wait for the service to send you an email with the check results.
+
+For docs, make sure you can do help(fun) and example(fun) on the R command line.
+
+For tests, you should read the submitted source code under tests/ directory, and assign a grade based on that.
+
+For "not-waiting" you should use "git checkout" to go back to the most recent commit on Feb 1/8 and then assign a grade based on reading the source code at that point.
+
+For "group-evals" we will do that in class tomorrow.
+
+Finally for extra credit, I noticed that one group did use openMP, and that -fopenmp causes a WARNING on win-builder, so for that WARNING I would say do not subtract any points from the win-builder section.
 
 devtools::install_github(
   "mertayD/Coding_Project_1_NN",
